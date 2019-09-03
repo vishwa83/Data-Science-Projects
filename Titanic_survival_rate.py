5175ec0f027260f365029ad9b492641682cf915b
vishwa83 <vishwa83@gmail.com>
2019-09-03 12:55:31 +0530

    Add files via upload

---
+++ Titanic_survival_rate.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+
+# In[2]:
+
+
+warnings.filterwarnings('ignore')
+
+
+# In[3]:
+
+
+data=pd.read_csv('titanic.csv')
+
+
+# # perform exploratory data analysis
+
+# In[5]:
+
+
+data.head()
+
+
+# In[6]:
+
+
+data.dtypes
+
+
+# In[7]:
+
+
+data.info
+
+
+# In[8]:
+
+
+data.corr()
+
+
+# In[12]:
+
+
+plt.
+sns.heatmap(data.corr())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
